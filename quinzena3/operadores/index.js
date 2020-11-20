641ec9170f26b72ae14194a35c84cf45e5ce29df
felipedev <feliipemonteiro901@gmail.com>
2020-11-19 22:06:56 -0300

    operadores

---
+++ quinzena3/operadores/index.js
@@ -0,0 +1,48 @@
+// 1. A. false  B. false C. false e. Boolean
+
+// 2. A. undefined B. null C. 11  D. 3 E. 19 F. undefined
+
+/* ///////////////////////////////////////////////////////////////// */
+/* Exercicio 01 
+<script>
+      var idadeUsuario = Number.parseInt(window.prompt('qual a sua idade: '))
+      var idadeAmigo = Number.parseInt(window.prompt('qual a idade do seu amigo??'))
+      var diferenca = idadeAmigo - idadeUsuario
+      window.prompt('Sua idade é maior do que a do seu melhor amigo?')
+      if( idadeUsuario > idadeAmigo){
+        window.prompt('true')
+      }else{
+        window.prompt('false')
+      }
+      window.prompt(`a diferença entra as idades é de ${diferenca} anos`)
+</script>
+*/
+
+/* ///////////////////////////////////////////////////////////////// */
+/* Exercicio 02 
+<script>
+      var idadeUsuario = Number.parseInt(window.prompt('qual a sua idade: '))
+      var idadeAmigo = Number.parseInt(window.prompt('qual a idade do seu amigo??'))
+      var diferenca = idadeAmigo - idadeUsuario
+      window.prompt('Sua idade é maior do que a do seu melhor amigo?')
+      if( idadeUsuario > idadeAmigo){
+        window.prompt('true')
+      }else{
+        window.prompt('false')
+      }
+      window.prompt(`a diferença entra as idades é de ${diferenca} anos`)
+</script>
+C. o padrão é que todo resto de divisão de números pares da 0
+D. vai da resto 1
+*/
+
+/* ///////////////////////////////////////////////////////////////// */
+/* Exercicio 04 */
+/* 
+<script>
+       var nomeUsuario = Text.toString(window.prompt('qual o seu nome?: '))
+       var email = Text.toString(window.prompt('qual o seu email?: '))
+       window.prompt(' Nome do Usúario é '+ nomeUsuario)
+       window.prompt(' Seu Email é '+ email)
+    </script>
+*/
